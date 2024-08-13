4b850693aad534998feaea7b1d992ee373b16f17
Bogomil-Simitchiev <simitchiev365@abv.bg>
2024-08-13 18:56:01 +0300

    Wrote 2 tests

--- state-management/src/app/app.component.spec.ts
+++ state-management/src/app/app.component.spec.ts
@@ -1,29 +1,47 @@
-import { TestBed } from '@angular/core/testing';
+import { ComponentFixture, TestBed } from '@angular/core/testing';
 import { AppComponent } from './app.component';
+import { select, Store } from '@ngrx/store';
+import { By } from '@angular/platform-browser';
+import { of } from 'rxjs';
+import { increment } from './+store';
 
-describe('AppComponent', () => {
-  beforeEach(async () => {
-    await TestBed.configureTestingModule({
+fdescribe('AppComponent', () => {
+  let component: AppComponent;
+  let fixture: ComponentFixture<AppComponent>;
+
+  beforeEach(() => {
+    TestBed.configureTestingModule({
       imports: [AppComponent],
-    }).compileComponents();
+      providers: [
+        {
+          provide: Store,
+          useValue: {
+            select: () => of(0),
+            dispatch: () => { }
+          }
+        }
+      ]
+    })
+
+    fixture = TestBed.createComponent(AppComponent);
+    component = fixture.componentInstance;
   });
 
-  it('should create the app', () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    const app = fixture.componentInstance;
-    expect(app).toBeTruthy();
-  });
-
-  it(`should have the 'state-management' title`, () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    const app = fixture.componentInstance;
-    expect(app.title).toEqual('state-management');
-  });
-
-  it('should render title', () => {
-    const fixture = TestBed.createComponent(AppComponent);
+  it('should show 0 as initial state', () => {
     fixture.detectChanges();
-    const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('h1')?.textContent).toContain('Hello, state-management');
+    const countContent = fixture.debugElement.query(By.css('div'));
+    expect(countContent).toBeTruthy();
+    expect(countContent.nativeElement.textContent.trim()).toEqual('Counter: 0');
   });
-});
+  it('should trigger action upon clicking increment', () => {
+    const store = TestBed.inject(Store);
+    spyOn(store, 'dispatch').and.callThrough();
+    fixture.detectChanges();
+    const incrementBtn = fixture.debugElement.query(By.css('[data-id="increment-btn"]'));
+
+    incrementBtn.triggerEventHandler('click', {});
+
+    expect(store.dispatch).toHaveBeenCalledWith(increment());
+
+  });
+})
